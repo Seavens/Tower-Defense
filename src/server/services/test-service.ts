b76a1c5abba445d9359f793eb37cd20be38228ed
Evan <32503947+Seavens@users.noreply.github.com>
2024-03-11 00:56:02 -0500

    Holy smokes!!!!

--- src/server/services/test-service.ts
+++ src/server/services/test-service.ts
@@ -1,23 +1,11 @@
 import { GenerateTower } from "shared/functions/tower-functions";
 import { Service } from "@flamework/core";
-import { inventorySlice } from "server/state/slices/inventory-slice";
+import { serverProducer } from "server/state/producer";
 import type { Entity } from "server/classes/entity";
 import type { InventoryAddItem } from "shared/state/actions";
 import type { OnPlayerAdded } from "./player-service";
 
 @Service({})
 export class TestService implements OnPlayerAdded {
-	public onPlayerAdded(player: Entity): void {
-		for (let i = 1; i < 7; i = i + 1) {
-			const tower = GenerateTower(player.id);
-			const inventoryItem: InventoryAddItem = {
-				item: tower,
-				slot: `${i}`,
-			};
-			warn(tower);
-
-			inventorySlice.inventoryAddItem(inventoryItem, { user: player.user, replicate: true });
-			inventorySlice.inventoryEquipItem(inventoryItem, { user: player.user, replicate: true });
-		}
-	}
+	public onPlayerAdded(player: Entity): void {}
 }
